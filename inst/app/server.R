69e7da1f80091b9af6884f8bdb3948531d75768a
Arnfinn <arnfinn.steindal@gmail.com>
2018-05-29 15:23:58 +0200

    Reorg. shiny ui; tried with some maps
    - Removed obsolete tabs (easier to test)
    - Included kommune shp and json file

--- inst/app/server.R
+++ inst/app/server.R
@@ -1,43 +1,65 @@
 library(shiny)
-library(ggplot2)
-library(leaflet)
+library(magrittr)
+
+#download.file(url = 'http://biogeo.ucdavis.edu/data/diva/adm/NOR_adm.zip',
+#              destfile = 'norway.zip')
+#download.file(url = 'http://norgeskart.no/json/norge/kommuner.json', destfile = 'inst/app/maps/kommuner.json')
+#unzip(zipfile = 'inst/app/maps/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.zip', exdir = "inst/app/maps")
+#unzip(zipfile = 'inst/app/maps/Basisdata_0000_Norge_25833_Kommuner_SOSI.zip', exdir = "inst/app/maps")
+
+norway <- maps::map(database = "world", regions = "Norway(?!:Svalbard)", fill = TRUE, col = 1, plot = F)
+norway2 <- maps::map(database = "world", regions = "Norway", fill = TRUE, col = 1, plot = F)
+
+topodata <- readLines("maps/kommuner.json") %>% paste(collapse = "\n")
+
+#norway3 <- rgdal::readOGR('maps/Kommune_FLATE.shp')
+norway3 <- rgdal::readOGR('maps/eldre.shp')
+
 
 shinyServer(
 
   function(input, output) {
-    
-    
+
+
     kartlagInput <- reactive({
       switch(input$kartlag,
              "Personer til fastlege/legevakt" = kols$FLLV_pers_Rate,
              "Personer poliklinikk" = kols$Poli_Pers_Rate,
              "Akuttinnlagte personer" = kols$Akutt_pers_Rate)
     })
-    
+
     output$kolstabell<-renderTable(
       kartlagInput()
-      )
+    )
 
     output$kolshisto <- renderPlot({
-      
+
       kolsdata <- data.frame(bohf=kols$Opptaksomr, kartlag=kartlagInput())
-      
+
       # barplot
-        ggplot(data=kolsdata, aes(x=reorder(bohf, kartlag), y=kartlag)) +
-        geom_bar(stat="identity", fill="#95BDE6") + 
-        labs(x = "Opptaksområde", y = input$kartlag) + 
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+      ggplot(data=kolsdata, aes(x=reorder(bohf, kartlag), y=kartlag)) +
+        geom_bar(stat="identity", fill="#95BDE6") +
+        labs(x = "Opptaksområde", y = input$kartlag) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
         theme(panel.background = element_blank())
-      
+
     })
-    
-    output$mymap <- renderLeaflet({
-      leaflet() %>%
-        addTiles() %>%  # Add default OpenStreetMap map tiles
-        addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
-      
+
+    output$map1 <- leaflet::renderLeaflet({
+
+      leaflet::leaflet(norway2) %>% leaflet::setView(lng = 10.0, lat = 65.0, zoom = 4) %>%
+        leaflet::addTiles() %>%
+        #        leaflet::addTopoJSON(topodata, weight = 1, color = "#444444", fill = FALSE) %>%
+        leaflet::addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
     })
-    
+
+    output$map2 <- shiny::renderPlot({
+      plot(norway3)
+    })
+
+    output$map3 <- shiny::renderPlot({
+      return(ggplot2::ggplot(ggplot2::geom_map(map = ggplot2::map_data("world", region = "Norway"))))
+    })
   }
-  
+
 )
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,27 +1,20 @@
 library(shiny)
-library(leaflet)
 
 shinyUI(function(request){
-  navbarPage(title = "",
-             tabPanel("Velg tema", 
-                      sidebarPanel(selectInput(inputId = "kartlag",
-                                          label = "Velg et tema:",
-                                          choices = c("Personer til fastlege/legevakt", 
-                                                      "Personer poliklinikk", 
-                                                      "Akuttinnlagte personer"))
-                      ),
-                      mainPanel(tableOutput("kolstabell")
-                        
-                      )
-             ),
-             
-             
-             
-             navbarMenu(title="Plots",
-                         tabPanel("kart", leafletOutput("mymap")),
+  fluidPage(title = "",
+            sidebarPanel(selectInput(inputId = "kartlag",
+                                     label = "Velg et tema:",
+                                     choices = c("Personer til fastlege/legevakt",
+                                                 "Personer poliklinikk",
+                                                 "Akuttinnlagte personer"))
+            ),
+            mainPanel(
+              tabsetPanel(
+                         tabPanel("leaflet", leaflet::leafletOutput("map1")),
+                         tabPanel("plot", shiny::plotOutput("map2")),
+                         tabPanel("ggplot2", shiny::plotOutput("map3")),
                          tabPanel("Kols histogram", plotOutput(outputId = "kolshisto"))
-             )
-
-            
+              )
+            )
   )
 })
